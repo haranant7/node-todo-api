cc1e1b539226ae204dc66a8d7ef6efe945dbae0f
Hariharan.A <hariharan.anantharaman89@gmail.com>
2018-01-14 15:24:17 +0530

    Add POST /todos and refactor mongoose

---
+++ server/server.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res)=>{
+    var todo = new Todo({
+        text : req.body.text
+    });
+
+     todo.save().then((doc) => {
+         res.send(doc);
+     },(e) => {
+         res.status(400).send(e);
+     });
+    console.log(req.body);
+});
+
+app.listen(3000,()=>{
+    console.log("Started on port 3000");
+});
+// var newTodo = new Todo({
+//     text : 'Cook dinner'
+// });
+//
+// newTodo.save().then((doc)=>{
+//     console.log('Saved Todo',doc);
+// },(e)=>{
+//     console.log('Unable to save Todo');
+// });
+
+// var otherTodo = new Todo({
+//     text : 'Edit this video'
+// });
+//
+// otherTodo.save().then((doc)=>{
+//     console.log('Saved Todo',doc);
+// },(e)=>{
+//     console.log('Unable to save Todo');
+// });
+
+
+
+// var user = new User({
+//     email:'user@example.com'
+// });
+//
+// user.save().then((doc)=>{
+//     console.log('User Saved',doc);
+// },(e)=>{
+//     console.log('Unable to save user',e);
+// });
